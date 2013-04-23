10f5e85a2d993e86d4fffe61be9d7df5bd63b05d
Hyungchan Kim <inlinechan@gmail.com>
2013-04-23 13:00:57 +0900

    Idle incremental update of gtags

--- init.el
+++ init.el
@@ -714,6 +714,47 @@ vi style of % jumping to matching brace."
                  (global-set-key (kbd "C-c g p") 'gtags-find-pattern)
                  (global-set-key (kbd "C-c g r") 'gtags-find-rtag)
                  (global-set-key (kbd "C-c g l") 'gtags-find-symbol)
+
+                 (defun global-update-incrementally () (shell-command "global -u -q")) ;; "*Messages*" "*Messages*") )
+
+                 ;; Call gtags update when idle for some time
+                 (defcustom my-gtags-update-idle-time 10
+                   "Number of idle seconds before an incremental gtags update is launched"
+                   :group 'my-group
+                   :type 'integer
+                   )
+
+                 ;; http://root42.blogspot.kr/2012/10/improved-handling-of-background-gnu.html
+                 ;; initially allow gtags updates
+                 (setq my-gtags-update-active t)
+
+                 (run-with-idle-timer my-gtags-update-idle-time t
+                                      (lambda ()
+                                        (if (and my-gtags-update-active
+                                                 (not (minibufferp) )
+                                                 )
+                                            (progn
+                                              (message "Running gtags...")
+                                              (global-update-incrementally)
+                                              )
+                                          )
+                                        )
+                                      )
+
+                 (add-hook 'ediff-quit-hook
+                           (lambda ()
+                             (message "Activating gtags update.")
+                             (setq my-gtags-update-active t)
+                             )
+                           )
+
+                 (add-hook 'ediff-before-setup-hook
+                           (lambda ()
+                             (message "Deactivating gtags update.")
+                             (setq my-gtags-update-active nil)
+                             )
+                           )
+
                  (message "gtags... done"))))
 
      (when section-jedi (message "jedi...")
