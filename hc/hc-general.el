60d5b3a73430ffd356ce589e59d4a898888fa905
Hyungchan Kim <inlinechan@gmail.com>
2013-10-28 10:27:09 +0900

    Add ido-recentf-open which was omitted while refactoring

--- hc/hc-general.el
+++ hc/hc-general.el
@@ -141,6 +141,16 @@ vi style of % jumping to matching brace."
 ;; enable recent files mode.
 (recentf-mode t)
 
+;; 50 files ought to be enough.
+(setq recentf-max-saved-items 50)
+
+(defun ido-recentf-open ()
+  "Use `ido-completing-read' to \\[find-file] a recent file"
+  (interactive)
+  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+      (message "Opening file...")
+    (message "Aborting")))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; notab
 (defun notab ()
