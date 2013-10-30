168109485ee6d2218101692558252793d588fea0
Hyungchan Kim <inlinechan@gmail.com>
2013-10-30 15:05:39 +0900

    Enable highlight trailing whitespaces only in some modes

--- el-get-init-files/init-highlight-chars.el
+++ el-get-init-files/init-highlight-chars.el
@@ -1,2 +1,15 @@
 (require 'highlight-chars)
-(add-hook 'font-lock-mode-hook 'hc-highlight-trailing-whitespace)
+;; (add-hook 'font-lock-mode-hook 'hc-highlight-trailing-whitespace)
+
+(dolist (mode (list
+               'c++-mode-hook
+               'c-mode-hook
+               'diff-mode-hook
+               'emacs-lisp-mode-hook
+               'git-commit-mode-hook
+               'js2-mode-hook
+               'python-mode-hook
+               ))
+  (add-hook mode 'hc-highlight-trailing-whitespace))
+
+(global-set-key (kbd "<f8>") 'hc-toggle-highlight-trailing-whitespace)
