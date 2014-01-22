9ce6cc439a2c5eb009e285fcd87f95edb4bfc647
Hyungchan Kim <inlinechan@gmail.com>
2014-01-22 15:19:36 +0900

    Unmap conflict M-] goto-match-paren in ggtags-mode

--- el-get-init-files/init-ggtags.el
+++ el-get-init-files/init-ggtags.el
@@ -2,4 +2,19 @@
 (add-hook 'c-mode-common-hook
           (lambda ()
             (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-              (ggtags-mode 1))))
+              (ggtags-mode 1)
+              )))
+
+;; Unmap M-]
+;; http://emacsredux.com/blog/2013/09/25/removing-key-bindings-from-minor-mode-keymaps/
+(defun my-ggtags-mode-hook()
+  (let ((oldmap (cdr (assoc 'ggtags-mode minor-mode-map-alist)))
+        (newmap (make-sparse-keymap)))
+    (message "%s" "init-ggtags")
+    (set-keymap-parent newmap oldmap)
+    (define-key newmap (kbd "M-]") nil)
+    (make-local-variable 'minor-mode-overriding-map-alist)
+    (push `(ggtags-mode . ,newmap) minor-mode-overriding-map-alist))
+)
+
+(add-hook 'ggtags-mode-hook 'my-ggtags-mode-hook)
