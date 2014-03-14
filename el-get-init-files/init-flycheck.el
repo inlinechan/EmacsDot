511a3ca8c6fbdefe3a15e392480b5ca58bf563b6
Hyungchan Kim <inlinechan@gmail.com>
2014-03-14 23:54:42 +0900

    Fix selecting checker in case of c++-mode
    Add new flycheck checker named hc/webkit-style which is able to detect
    its buffer-file-name by using :predicate.

--- el-get-init-files/init-flycheck.el
+++ el-get-init-files/init-flycheck.el
@@ -5,86 +5,84 @@
 
 (require 'flycheck)
 
+(setq verbose t)
+
 (defun hc/get-webkit-dir ()
   "Return WebKit dir under $HOME."
-  (setq home-dir (getenv "HOME"))
-  (dolist (dir (list "QtWebKit" "WebKit" "src/WebKit"))
-    (setq full-path (concat home-dir "/" dir))
-    (when (file-exists-p full-path)
-      (return full-path))))
+  (let ((home-dir (getenv "HOME")))
+    (dolist (dir (list "QtWebKit" "WebKit" "src/WebKit"))
+      (setq full-path (concat home-dir "/" dir))
+      (when (file-exists-p full-path)
+        (return full-path)))))
 
 (defun hc/get-check-script-path ()
   "Return check-webkit-style script."
-  (concat (hc/get-webkit-dir) "/" "Tools/Scripts/check-webkit-style"))
+  (let ((webkit-dir (hc/get-webkit-dir)))
+    (when webkit-dir
+      (concat webkit-dir "/" "Tools/Scripts/check-webkit-style"))))
 
-(setq check-webkit-style-path (hc/get-check-script-path))
-
-(flycheck-define-checker hc/webkit-style
-  "A syntax checker for WebKit.
+(defun hc/register-webkit-style-checker (script-path)
+  (when script-path
+    (flycheck-define-checker hc/webkit-style
+      "A syntax checker for WebKit.
 
 See URL `http://www.webkit.org/coding/coding-style.html'."
-  :command ("python"
-            (eval check-webkit-style-path)
-            source)
-  :error-patterns
-  ((warning line-start (file-name) ":" line ":" (message) line-end))
-  :modes (c++-mode)
-  :next-checkers ((warnings-only . c/c++-cppcheck)))
+      :command ("python"
+                (eval (hc/get-check-script-path))
+                source)
+      :error-patterns
+      ((warning line-start (file-name) ":" line ":" (message) line-end))
+      :modes (c++-mode)
+      :next-checkers ((warnings-only . c/c++-cppcheck))
+      :predicate
+      (lambda ()
+        (when verbose
+          (message "#### hc/webkit-style predicate %s" buffer-file-name))
+        (and buffer-file-name
+             (hc/get-check-script-path)
+             (or (string-match "WebCore" buffer-file-name)
+                 (string-match "JavaScriptCore" buffer-file-name)
+                 (string-match "WebKit" buffer-file-name)
+                 (string-match "WebKit2" buffer-file-name)))))
+    (add-to-list 'flycheck-checkers 'hc/webkit-style)))
 
-(defun hc/flycheck-c++-mode-select-checker ()
-  "Select check based on buffer name"
-  (if (or (string-match "WebCore" buffer-file-name)
-          (string-match "JavaScriptCore" buffer-file-name)
-          (string-match "WebKit" buffer-file-name)
-          (string-match "WebKit2" buffer-file-name))
-      (flycheck-select-checker 'hc/webkit-style)
-    (when (executable-find "clang++")
-      (flycheck-select-checker 'c/c++-clang))))
 
-(defun hc/add-flycheck-mode-hook ()
-  "Enable flycheck-mode if necessary."
+(defun hc/flycheck-add-mode-hook ()
+  "Enable flycheck in some modes"
   (interactive)
-  ;; Enable check-webkit-style only for WebKit source code
-  (when (eq major-mode 'c++-mode)
-    (add-hook 'c++-mode-hook 'hc/flycheck-c++-mode-select-checker))
-  (add-hook 'sh-mode-hook 'flycheck-mode)
-  (add-hook 'emacs-lisp-mode-hook 'flycheck-mode)
-  (add-hook 'html-mode-hook 'flycheck-mode)
-  (add-hook 'python-mode-hook 'flycheck-mode))
+  (mapcar #'(lambda (mode) (add-hook mode 'flycheck-mode))
+          '(c++-mode-hook
+            sh-mode-hook
+            emacs-lisp-mode-hook
+            html-mode-hook
+            python-mode-hook)))
 
-(defun hc/flycheck-add-default-mode-hook ()
-  "Add default mode for flycheck."
-  (add-hook 'sh-mode-hook 'flycheck-mode)
-  (add-hook 'emacs-lisp-mode-hook 'flycheck-mode)
-  (add-hook 'html-mode-hook 'flycheck-mode)
-  (add-hook 'python-mode-hook 'flycheck-mode))
-
-(defun hc/remove-flycheck-mode-hook ()
-  "Remove flycheck mode hook."
+(defun hc/flycheck-remove-mode-hook ()
+  "Disable flycheck in some modes"
   (interactive)
-  ;; Disable flycheck mode check-webkit-style in WebKit buffer
-  (when (eq major-mode 'c++-mode)
-    (remove-hook 'c++-mode-hook 'hc/flycheck-c++-mode-select-checker))
-  (remove-hook 'sh-mode-hook 'flycheck-mode)
-  (remove-hook 'emacs-lisp-mode-hook 'flycheck-mode)
-  (remove-hook 'html-mode-hook 'flycheck-mode)
-  (remove-hook 'python-mode-hook 'flycheck-mode))
+  (mapcar #'(lambda (mode) (remove-hook mode 'flycheck-mode))
+          '(c++-mode-hook
+            sh-mode-hook
+            emacs-lisp-mode-hook
+            html-mode-hook
+            python-mode-hook)))
 
 (defun hc/toggle-flycheck ()
   "Toggle flycheck mode."
   (interactive)
-  ;; (message "%s %s" "flycheck-mode" flycheck-mode)
+  (when verbose
+    (message "%s %s" "flycheck-mode" flycheck-mode))
   (let ((old-mode flycheck-mode))
     (if old-mode
-        (hc/remove-flycheck-mode-hook)
-      (hc/add-flycheck-mode-hook))
+        (hc/flycheck-remove-mode-hook)
+      (hc/flycheck-add-mode-hook))
     (when flycheck-mode
       (setq current-prefix-arg nil))
     (call-interactively 'flycheck-mode)))
 
-(add-to-list 'flycheck-checkers 'hc/webkit-style)
-(hc/flycheck-add-default-mode-hook)
+(hc/register-webkit-style-checker (hc/get-check-script-path))
 ;; (add-hook 'after-init-hook #'global-flycheck-mode)
+(hc/flycheck-add-mode-hook)
 
 (global-set-key (kbd "C-<f8>") 'hc/toggle-flycheck)
 
