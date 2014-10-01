6cf021c04ffdcb8b354e0f36b18a52954b1e8790
Hyungchan Kim <hyungchan2.kim@lge.com>
2014-10-01 16:40:05 +0900

    Add missing key binding such as M-* globally

--- el-get-init-files/init-gtags.el
+++ el-get-init-files/init-gtags.el
@@ -18,11 +18,20 @@
              (define-key gtags-select-mode-map "j" 'next-line)
              (define-key gtags-select-mode-map "p" 'previous-line)
              (define-key gtags-select-mode-map "n" 'next-line)
+             (define-key gtags-select-mode-map "\ep" 'previous-line)
+             (define-key gtags-select-mode-map "\en" 'next-line)
              (define-key gtags-select-mode-map "\e*" 'gtags-pop-stack)
              (define-key gtags-select-mode-map "q" 'gtags-pop-stack)
              (define-key gtags-select-mode-map "u" 'gtags-pop-stack)
              (define-key gtags-select-mode-map "\C-o" 'gtags-select-tag-other-window)
+             (define-key gtags-select-mode-map "\eo" 'gtags-select-tag-other-window-focus)
              (define-key gtags-select-mode-map "\e." 'gtags-select-tag)))
+
+(add-hook 'gtags-mode-hook
+          '(lambda ()
+             (define-key gtags-mode-map "\e*" 'gtags-pop-stack)
+             (define-key gtags-mode-map "\e." 'gtags-find-tag)
+             (define-key gtags-mode-map "\C-x4." 'gtags-find-tag-other-window)))
 
 (setq gtags-path-style 'relative)
 ;; (setq gtags-auto-update t)
