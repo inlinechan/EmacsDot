43421238c9d6b11267e04c6cfc56c3496048eb9f
Hyungchan Kim <hyungchan2.kim@lge.com>
2014-10-24 08:09:24 +0900

    Fix broken tab completion in term mode (disable yasnippet)

--- el-get-init-files/init-yasnippet.el
+++ el-get-init-files/init-yasnippet.el
@@ -1,3 +1,7 @@
 ;; (add-to-list 'load-path "~/.emacs.d/el-get/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
+
+;; http://stackoverflow.com/questions/18278310/emacs-ansi-term-not-tab-completing
+(add-hook 'term-mode-hook
+          (lambda() (yas-minor-mode -1)))
