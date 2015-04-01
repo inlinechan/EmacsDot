23fbab3f05c392554bec872bacddf40f73565717
Hyungchan Kim <inlinechan@gmail.com>
2015-04-01 11:26:57 +0900

    Adjust tag color in magit log mode when non window-system
    Without this, can't see tag name in magit log mode

--- el-get-init-files/init-magit.el
+++ el-get-init-files/init-magit.el
@@ -3,3 +3,8 @@
 
 (require 'magit)
 (global-set-key (kbd "C-c s") 'magit-status)
+
+(eval-after-load 'magit
+  '(progn
+     (when (not window-system)
+       (set-face-background 'magit-log-head-label-tags "Grey85"))))
