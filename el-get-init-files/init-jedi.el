a2fe12d463a51f6a2fdc636f3fa6e7c89a90bf47
Hyungchan Kim <inlinechan@gmail.com>
2016-12-05 08:59:39 +0900

    Support python3 for jedi and flycheck

--- el-get-init-files/init-jedi.el
+++ el-get-init-files/init-jedi.el
@@ -3,6 +3,11 @@
 (setq jedi:key-goto-definition (kbd "C-c ."))
 (require 'jedi)
 
+(setq jedi:environment-root "jedi")  ; or any other name you like
+(setq jedi:environment-virtualenv
+      (append python-environment-virtualenv
+              '("--python" "/usr/bin/python3")))
+
 ;; python-mode-hook
 (add-hook 'python-mode-hook 'jedi:setup)
 (add-hook 'python-mode-hook 'jedi:ac-setup)
