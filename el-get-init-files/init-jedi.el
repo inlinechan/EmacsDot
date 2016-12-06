41868a3303cafe95047d7218e8639912187c0058
Hyungchan Kim <inlinechan@gmail.com>
2016-12-06 13:36:02 +0900

    Use virtualenv python in jedi
    TODO:
    Support per directory or project setting

--- el-get-init-files/init-jedi.el
+++ el-get-init-files/init-jedi.el
@@ -1,14 +1,19 @@
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
 (setq jedi:key-goto-definition (kbd "C-c ."))
-(require 'jedi)
 
-(setq jedi:environment-root "jedi")  ; or any other name you like
-(setq jedi:environment-virtualenv
-      (append python-environment-virtualenv
-              '("--python" "/usr/bin/python3")))
-
-;; python-mode-hook
-(add-hook 'python-mode-hook 'jedi:setup)
-(add-hook 'python-mode-hook 'jedi:ac-setup)
-(add-hook 'python-mode-hook 'jedi:start-server)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (require 'python-environment)
+            (require 'jedi)                          ; to expose `jedi:start-server'
+            (setq jedi:environment-root "python3.5")  ; or any other name you like
+            (setq jedi:environment-virtualenv
+                  (append python-environment-virtualenv
+                          '("--python"
+                            (concat (file-name-as-directory python-environment-directory) jedi:environment-root "/bin/python"))))
+            (setq jedi:server-command
+                  (list (concat (file-name-as-directory python-environment-directory) jedi:environment-root "/bin/jediepcserver")))
+            (jedi:setup)
+            (jedi:ac-setup)
+            (jedi:start-server)
+            ))
