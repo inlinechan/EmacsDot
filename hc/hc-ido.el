9085981308001dff6a79b908ea287587bf40f181
Hyungchan Kim <inlinechan@gmail.com>
2017-03-07 10:10:46 +0900

    Ido shows .org first .html later

--- hc/hc-ido.el
+++ hc/hc-ido.el
@@ -5,6 +5,6 @@
 ;; this setting will force Ido to always create a new buffer (in C-x b) if the name does not exist
 (setq ido-create-new-buffer 'always)
 
-(setq ido-file-extensions-order '(".cc" ".cpp" ".c" ".h" ".txt"))
+(setq ido-file-extensions-order '(".cc" ".cpp" ".c" ".h" ".txt" ".org" ".html"))
 
 (provide 'hc-ido)
