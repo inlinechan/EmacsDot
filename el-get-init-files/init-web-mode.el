51f5933956ae85dbaa481c84ef31f38f7389350a
Hyungchan Kim <inlinechan@gmail.com>
2017-03-07 10:08:07 +0900

    Setup jsx in web-mode

--- el-get-init-files/init-web-mode.el
+++ el-get-init-files/init-web-mode.el
@@ -1,1 +1,11 @@
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+(setq web-mode-markup-indent-offset 2
+      web-mode-css-indent-offset 2
+      web-mode-code-indent-offset 2
+      s-indent-level 2)
+
+;; Enable this if .js highlight like .jsx
+;; (setq web-mode-content-types-alist
+;;   '(("jsx" . "\\.js[x]?\\'")))
